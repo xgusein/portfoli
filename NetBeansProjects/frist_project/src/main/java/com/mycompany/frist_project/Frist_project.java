8e8772711386e267fe2ce1c4eace6ffd22db0d22
xgusein <ibadzadehuseyn@gmail.com>
2023-03-05 19:17:04 +0400

    frist_application

---
+++ NetBeansProjects/frist_project/src/main/java/com/mycompany/frist_project/Frist_project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.frist_project;
+
+/**
+ *
+ * @author huseynibadzade
+ */
+public class Frist_project {
+
+    public static void main(String[] args) {
+        System.out.print("Salam");
+        System.out.println("welcome");
+    }
+}
